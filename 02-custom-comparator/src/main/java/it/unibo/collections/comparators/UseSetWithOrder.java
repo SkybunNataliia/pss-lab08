d9fdf75309a806e497026e397ec18b61d800e7dd
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-26 11:31:11 +0100

    Implemented function that use ordered tree set

--- 02-custom-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
+++ 02-custom-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
@@ -1,5 +1,6 @@
 package it.unibo.collections.comparators;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -25,12 +26,18 @@ public final class UseSetWithOrder {
          * The comparator must convert the strings to double, then compare the doubles to find the biggest.
          * The comparator does not need to deal with the case of Strings which are not parseable as doubles.
          */
+        final Collection<String> tree = new TreeSet<String>(new StringByDoubleBigger());
         /*
          * 2) Inserts in the set a hundred strings containing randomly generated numbers
          * (use Math.random(), and convert to String appropriately)
          */
+        for (int i = 1; i <= 100; i++) {
+            final var num = Math.random();
+            tree.add(String.valueOf(num));
+        }
         /*
          * 3) Prints the set, which must be ordered
          */
+        System.out.println("Ordered tree set: " + tree);
     }
 }
